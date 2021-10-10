c026a81f1aec9b319a7b55a4b28fa64b84618b62
Fábio Veiga <veigabfabio@hotmail.com>
2021-10-10 13:38:34 -0300

    version 1.1.3

--- daycyclediscord/download/.old/DayCyleDiscord.plugin.js
+++ daycyclediscord/download/.old/DayCyleDiscord.plugin.js
@@ -2,7 +2,7 @@
  * @name DayCycleDiscord
  * @author v37ga
  * @description changes your wallpaper depending on what time is it.
- * @version 1.1.2
+ * @version 1.1.0
  * @invite inviteCode
  * @authorId 368194347222695938
  * @authorLink https://twitter.com/veigaffa
