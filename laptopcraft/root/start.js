160a96a0c5942bed45837c88200e470a0a104f8f
Fábio Veiga <veigabfabio@hotmail.com>
2022-04-04 04:21:38 -0300

    Update start.js

--- laptopcraft/root/start.js
+++ laptopcraft/root/start.js
@@ -10,22 +10,17 @@
     console.log(itens)
 
     for (let i = 0; i <= (itens.length - 1); i++){
-        /*function fimAnimacao(e){
+        function fimAnimacao(e){
             e.addEventListener("animationend", () =>{
                 itens[i].classList.add("show");
                 itens[i].classList.remove("hidden")       
             })
-        }*/
+        }
         if (i == 0){
-            blocoCentral.addEventListener("animationend", () =>{
-                itens[i].classList.add("show");
-                itens[i].classList.remove("hidden")       
-            })
+            fimAnimacao(blocoCentral)     
+            
         }else{
-            itens[i-1].addEventListener("animationend", () =>{
-                itens[i].classList.add("show");
-                itens[i].classList.remove("hidden")       
-            })
+            fimAnimacao(itens[i]-1)
         }  
     }
 
