73f8544801249fabd4582c39a6c703e53c892235
Fábio Veiga <veigabfabio@hotmail.com>
2022-04-04 15:12:50 -0300

    ...

--- laptopcraft/root/navMenu.js
+++ laptopcraft/root/navMenu.js
@@ -8,28 +8,22 @@ const elementos = [blocoCentralTitulo,blocoCentralNav,hr[0]];
     console.log(elementos)
 function trocaSessao(e){
     //1 = home para downloads.
-    
+    document.title = "LapTopCraft | Downloads"
     if(e == 1){
         blocoCentralTitulo.addEventListener("animationend", moveup)
-        
         console.log("done")
     }else{
 
     }
 
     for (let i = 0;i <= elementos.length - 1; i++){
-            switch(e){
-            case 1:
-                slime.classList.add("moveup")
-                elementos[i].classList.add("fadeout");
-            break;
-        }
+        slime.classList.add("moveup")
+        elementos[i].classList.add("fadeout");       
     }
 
     
     function moveup(){
         console.log("entra")
-        var slime = document.querySelector(".slime")
         slime.classList.add("moveup")
         slime.addEventListener("animationend", apaga())
         slime.children[0].style.margin = "90px 0 0 0"
@@ -42,7 +36,23 @@ function trocaSessao(e){
             console.log("teste>"+index)
             index++
         }
+    cria()
     }
-
-
+    function cria(){
+        $("<a>", { text: "VOLTAR", href: "#"}).appendTo(".voltar");
+        const span = document.querySelector(".voltar")
+        span.children[0].classList.add("botaoVolta")
+        
+        span.addEventListener("click", ()=>{
+            slime.classList.remove("moveup")
+            slime.classList.remove("show")
+            slime.classList.add("fadeout")
+            span.classList.add("fadeout")
+            
+        })
+        span.addEventListener("animationend", () => {
+            slime.remove()
+            window.open("../index.html","_self")
+        })
+    }
 }
